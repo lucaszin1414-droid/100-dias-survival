d207551424e9985318c20efa109cbdb0211beb64
Lucas <lucaszin1414@gmail.com>
2026-01-19 08:14:19 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "sobreviva-100-dias-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+  // Se tiver ícones ou outros assets, inclua aqui
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
