2a969096f1f24fd3b193758bbfb283b8b917b5c9
Lucas <lucaszin1414@gmail.com>
2026-01-19 13:04:42 -0300

    Update2 sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,20 @@
-const CACHE_NAME = '100dias-cache-v3'; // altere a versão sempre que atualizar o código
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  // inclua aqui css, js, imagens
-];
-
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting()) // força ativação imediata
+    caches.open(CACHE_NAME).then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.json',
+      // CSS/JS/imagens
+    ])).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then(keys => {
       return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       );
     }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
   );
 });
