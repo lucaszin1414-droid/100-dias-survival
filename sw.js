d4e1dc47f77d43157cd6dd3c4ef187b28bea4123
Lucas <lucaszin1414@gmail.com>
2026-01-20 07:37:23 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,20 @@
-const CACHE_NAME = "100dias-cache-v7";
+const CACHE_NAME = "100dias-v002";
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json"
-      ])
-    ).then(() => self.skipWaiting())
-  );
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    ).then(() => self.clients.claim())
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
